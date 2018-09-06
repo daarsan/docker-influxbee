9566ef6ae987e182e0ea7c0c788475095ee0d7a5
David Aroca <d.aroca.sanchez@gmail.com>
2018-09-06 21:42:16 +0200

    First version

---
+++ influxbee.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+source /influxbee/venv/bin/activate
+
+python influxbee.py -u $MIRUBEE_USER -p ${MIRUBEE_PASSWORD} --config-file /config/mirubee.toml --influxdb-host ${INFLUXDB_HOST} --influxdb-port ${INFLUXDB_PORT} --influxdb-database ${INFLUXDB_DATABASE} --influxdb-user ${INFLUXDB_USER} --influxdb-password ${INFLUXDB_PASSWORD} --requests-per-day ${REQUESTS_PER_DAY}
